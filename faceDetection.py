db829dc32b696e0efe6eb04c3ce2c5f024331f35
jongsub100 <71091406+jongsub100@users.noreply.github.com>
2020-12-09 16:07:55 +0900

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -1,6 +1,5 @@
   GNU nano 3.2                    faceDetection.py                              
 
-    img = cv2.flip(img, -1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
         gray,
